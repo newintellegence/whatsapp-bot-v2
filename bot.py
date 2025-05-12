9b65cf3147dcff09e01b8a42031842a4df95ac31
Abdulrahman <newstats25@gmail.com>
2025-05-12 09:28:22 +0300

    Fixed Render port issue

--- bot.py
+++ bot.py
@@ -23,4 +23,5 @@ def whatsapp_reply():
     return str(response)
 
 if __name__ == "__main__":
-    app.run(port=5000)
+    port = int(os.environ.get("PORT", 5000))  # <-- Change here
+    app.run(host="0.0.0.0", port=port)
